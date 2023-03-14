62f93b25abd544514c9c0fffb940fe563a1388a3
Christopher <chris.brower.96@gmail.com>
2023-03-14 10:32:47 -0600

    commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    i=0
+    while i < length:
+        if(i==0):
+            fib.append(0)
+            
+        elif(i==1):
+            fib.append(1)
+        else:
+            fib.append(fib[i-1]+fib[i-2])
+        i+=1
+    print(fib)
+    return fib
+print_fibonacci(9)
